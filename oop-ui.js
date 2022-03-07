5725fa5ba11609fff87d50ab56f0179e175061be
Mohammed Che <mocherbatji@gmail.com>
2022-03-07 18:50:02 +0000

    Update oop-ui.js

--- oop-ui.js
+++ oop-ui.js
@@ -1,9 +1,17 @@
+if(studentSettings.clearStorage ?? true){
+  console.log("STORAGE CLEARED");
+  localStorage.clear();
+}
+
 const title = document.querySelector("#ui_title");
 const subtitle = document.querySelector("#ui_subtitle");
 const studentName = document.querySelector("#ui_student_name");
 const studentNumber = document.querySelector("#ui_student_number");
 const stockHead = document.querySelector("#ui_stock_table_head");
 const stockBody = document.querySelector("#ui_stock_table_body");
+const stockRefresh = document.querySelector("#ui_stock_table_refresh");
+
+const REFRESH_RATE = studentSettings.tableRefreshRate ?? 5;
 
 title.innerHTML = studentSettings.projectTitle;
 // subtitle.innerHTML = studentDetails.studentName;
@@ -14,10 +22,18 @@ studentNumber.innerHTML = studentSettings.studentNumber;
 
 
 let tableMaker = (storageProperty) => {
-
   let table = JSON.parse(localStorage.getItem(storageProperty));
   let headings = '';
   let rows = '';
+  
+  if(!table){
+    return {
+      headings,
+      rows
+    }
+  }
+
+  
   let products = Object.keys(table[0]);
   console.log("UI stock");
   console.log(products);
@@ -44,6 +60,23 @@ let tableMaker = (storageProperty) => {
 
 
 //Stock table
-let stockTable = tableMaker('stock');
-stockHead.innerHTML = stockTable.headings;
-stockBody.innerHTML = stockTable.rows;
+function makeStockTable(){
+  let stockTable = tableMaker('stock');
+  stockHead.innerHTML = stockTable.headings;
+  stockBody.innerHTML = stockTable.rows;  
+}
+
+let count = REFRESH_RATE + 1;
+function stockTableCounter(){
+  count--;
+  if(count <= 0){
+    count = REFRESH_RATE;
+  }
+  stockRefresh.innerHTML = count;
+}
+
+makeStockTable();
+stockTableCounter();
+
+setInterval(makeStockTable, REFRESH_RATE*1000);
+setInterval(stockTableCounter, 1000);
