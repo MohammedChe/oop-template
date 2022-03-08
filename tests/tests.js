439160ae7f78ce295d8d6d46ed8eec998977ee92
Mohammed Che <mocherbatji@gmail.com>
2022-03-08 22:35:02 +0000

    Create tests.js

---
+++ tests/tests.js
@@ -0,0 +1,19 @@
+describe("Index", function () {
+
+  describe("#Class", function () {
+    it("should return 42", function () {
+      const result = 50;
+      chai.expect(result).to.equal(42);
+    });
+  });
+});
+
+describe("Index2", function () {
+
+  describe("#Class", function () {
+    it("should return 42", function () {
+      const result = 42;
+      chai.expect(result).to.equal(42);
+    });
+  });
+});
